1f624ce5e6cee1ef3fdf8d421401ccb080cd9b5b
suhyeon <ksuhy0507@gmail.com>
2025-05-14 13:50:59 +0900

    250514_modified

--- opencvtest/HighPassFilter.cpp
+++ opencvtest/HighPassFilter.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-//int main() {
-//    cv::Mat original_img, result_img;
-//
-//    original_img = cv::imread("./tibetfox.bmp", cv::IMREAD_GRAYSCALE);
-//    result_img.create(cv::Size(original_img.cols - 2, original_img.rows - 2), CV_8UC1);
-//
-//    float LPF[3][3] = {
-//        {-1, -1, -1},
-//        {-1, 8, -1},
-//        {-1, -1, -1}
-//    };
-//
-//    for (int y = 0; y < result_img.rows; y++) {
-//        for (int x = 0; x < result_img.cols; x++) {
-//            float sum = 0;
-//            for (int dy = 0; dy <= 2; dy++) {
-//                for (int dx = 0; dx <= 2; dx++) {
-//                    sum += original_img.at<uchar>(y + dy, x + dx) * LPF[dy][dx];
-//                }
-//            }
-//            result_img.at<uchar>(y, x) = cv::saturate_cast<uchar>(sum);
-//        }
-//    }
-//
-//    cv::imshow("original Image", original_img);
-//    cv::imshow("result Image", result_img);
-//    cv::waitKey(0);
-//
-//    cv::imwrite("tibetfox_hpf.jpg", result_img);
-//
-//    return 0;
-//
-//}
+int main() {
+    cv::Mat original_img, result_img;
+
+    original_img = cv::imread("./tibetfox.bmp", cv::IMREAD_GRAYSCALE);
+    result_img.create(cv::Size(original_img.cols - 2, original_img.rows - 2), CV_8UC1);
+
+    float LPF[3][3] = {
+        {-1, -1, -1},
+        {-1, 8, -1},
+        {-1, -1, -1}
+    };
+
+    for (int y = 0; y < result_img.rows; y++) {
+        for (int x = 0; x < result_img.cols; x++) {
+            float sum = 0;
+            for (int dy = 0; dy <= 2; dy++) {
+                for (int dx = 0; dx <= 2; dx++) {
+                    sum += original_img.at<uchar>(y + dy, x + dx) * LPF[dy][dx];
+                }
+            }
+            result_img.at<uchar>(y, x) = cv::saturate_cast<uchar>(sum);
+        }
+    }
+
+    cv::imshow("original Image", original_img);
+    cv::imshow("result Image", result_img);
+    cv::waitKey(0);
+
+    cv::imwrite("tibetfox_hpf.jpg", result_img);
+
+    return 0;
+
+}
--- opencvtest/toblack.cpp
+++ opencvtest/toblack.cpp
@@ -2,47 +2,47 @@
 #include "opencv2/opencv.hpp"
 
 // 이진화 구현
-//int main(void)
-//{
-//	
-//	cv::Mat image;
-//	image = cv::imread("cat.bmp", cv::IMREAD_GRAYSCALE);
-//
-//
-//	// 이미지 로드에 실패할 경우를 에러 메세지 반환
-//	if (image.empty())
-//	{
-//		std::cout << "image load fail" << std::endl;
-//
-//		return -1;
-//	}
-//
-//
-//	for (int y = 0; y < image.rows; y++)
-//	{
-//		for (int x = 0; x < image.cols; x++)
-//		{
-//			int tmp = image.at<uchar>(y, x);
-//
-//			if (image.at<uchar>(y, x) >= 128)
-//			{				
-//				tmp = 255;
-//			}
-//			else
-//			{
-//				tmp = 0;
-//			}
-//
-//			image.at<uchar>(y, x) = cv::saturate_cast<uchar>(tmp);
-//			
-//			
-//
-//		}
-//	}
-//
-//	// 이미지 출력
-//	cv::imshow("Original Image", image);
-//	cv::waitKey(0);
-//
-//	return 0;
-//}
+int main(void)
+{
+	
+	cv::Mat image;
+	image = cv::imread("cat.bmp", cv::IMREAD_GRAYSCALE);
+
+
+	// 이미지 로드에 실패할 경우를 에러 메세지 반환
+	if (image.empty())
+	{
+		std::cout << "image load fail" << std::endl;
+
+		return -1;
+	}
+
+
+	for (int y = 0; y < image.rows; y++)
+	{
+		for (int x = 0; x < image.cols; x++)
+		{
+			int tmp = image.at<uchar>(y, x);
+
+			if (image.at<uchar>(y, x) >= 128)
+			{				
+				tmp = 255;
+			}
+			else
+			{
+				tmp = 0;
+			}
+
+			image.at<uchar>(y, x) = cv::saturate_cast<uchar>(tmp);
+			
+			
+
+		}
+	}
+
+	// 이미지 출력
+	cv::imshow("Original Image", image);
+	cv::waitKey(0);
+
+	return 0;
+}
